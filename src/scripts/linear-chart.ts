a2a39893cbe6c09cd6376b4d1562fe0ec6c65e25
jaimytaco <jaimy.taco@gmail.com>
2022-04-18 02:26:40 -0500

    fix: styles in linear-chart

--- src/scripts/linear-chart.ts
+++ src/scripts/linear-chart.ts
@@ -5,15 +5,24 @@ import am5themes_Animated from '@amcharts/amcharts5/themes/Animated'
 export function getLinearChart(id: string) {
     const root = am5.Root.new(id)
 
+    const myTheme = am5.Theme.new(root);
+
+    myTheme.rule("Label").setAll({
+        fill: am5.color('#fff'),
+        fontFamily: 'Poppins'
+    });
+
     root.setThemes([
-        am5themes_Animated.new(root)
+        am5themes_Animated.new(root),
+        myTheme
     ])
 
     const chart = root.container.children.push(am5xy.XYChart.new(root, {}))
 
     const cursor = chart.set('cursor', am5xy.XYCursor.new(root, {
-        behavior: 'none'
+        behavior: 'none',
     }))
+    cursor.lineX.set('visible', false)
     cursor.lineY.set('visible', false)
 
     const date = new Date()
@@ -25,7 +34,8 @@ export function getLinearChart(id: string) {
         am5.time.add(date, 'day', 1)
         return {
             date: date.getTime(),
-            value: value
+            value: value,
+            dateStr: date.toLocaleDateString('en-us', { year: 'numeric', month: 'short', day: 'numeric' })
         }
     }
 
@@ -44,16 +54,18 @@ export function getLinearChart(id: string) {
             count: 1
         },
         renderer: am5xy.AxisRendererX.new(root, {
-            pan: 'zoom'
+            pan: 'none',
         }),
-        tooltip: am5.Tooltip.new(root, {})
+        tooltip: am5.Tooltip.new(root, {}),
     }))
 
     const yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
         maxDeviation: 1,
         renderer: am5xy.AxisRendererY.new(root, {
-            pan: 'zoom'
-        })
+            pan: 'none'
+        }),
+        extraMax: 1,
+        cursorTooltipEnabled: false
     }))
 
     const series = chart.series.push(am5xy.SmoothedXLineSeries.new(root, {
@@ -63,29 +75,44 @@ export function getLinearChart(id: string) {
         valueYField: 'value',
         valueXField: 'date',
         tooltip: am5.Tooltip.new(root, {
-            // labelText: 'Popularidad {valueY}'
-            labelText: "Popularity\n[bold]{valueY}[/]\nMay 10th, 2021"
-        })
+            labelText: "Popularity\n[bold]{valueY}[/]\n{dateStr}",
+            dy: -12
+        }),
+        fill: am5.color('#3A3A3C'),
+        stroke: am5.color('#00C26C'),
+        tension: 0
     }))
 
-    series.fills.template.setAll({
-        visible: true,
-        fillOpacity: 0.2
-    })
-
     series.bullets.push(function () {
-        return am5.Bullet.new(root, {
-            locationY: 0,
-            sprite: am5.Circle.new(root, {
-                radius: 4,
-                stroke: root.interfaceColors.get('background'),
-                strokeWidth: 2,
-                fill: series.get('fill')
-            })
+        const sprite = am5.Circle.new(root, {
+            radius: 2,
+            stroke: root.interfaceColors.get('background'),
+            strokeWidth: 4,
+            fill: am5.color('#5E5CE6'),
+            interactive: true,
+            opacity: .2
         })
+
+        sprite.states.create('hover', {
+            radius: 8,
+            opacity: 2
+        });
+
+        sprite.states.create('default', {
+            radius: 2,
+            opacity: .2
+        });
+
+        const bullet = am5.Bullet.new(root, {
+            locationY: 0,
+            sprite
+        })
+
+        return bullet
     })
 
     const data = generateDatas(50)
+    console.log('data =', data)
     series.data.setAll(data)
 
     series.appear(1000)
