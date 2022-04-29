934ce304def987dc7cd2855bf54d7940f01ece8a
jaimytaco <jaimy.taco@gmail.com>
2022-04-28 19:30:59 -0500

    feat: integration with api & re-render charts according to data

--- src/scripts/main.ts
+++
@@ -1,6 +0,0 @@
-import '../style.css'
-import { getCountryChart } from './country-chart'
-import { getLinearChart } from './linear-chart'
-
-getCountryChart('country-chart')
-getLinearChart('linear-chart')
